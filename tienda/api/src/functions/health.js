aba9733b961db2b7cc7c902f7b7248f49dddc6fe
apenitas <jquihuisarriola@gmail.com>
2026-07-11 21:30:21 -0700

    Create health.js

---
+++ tienda/api/src/functions/health.js
@@ -0,0 +1,17 @@
+const { app } = require("@azure/functions");
+
+app.http("health", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  route: "health",
+
+  handler: async () => {
+    return {
+      status: 200,
+      jsonBody: {
+        ok: true,
+        message: "La API funciona correctamente"
+      }
+    };
+  }
+});
